aa22feaeaad8ff0e95a1795d35ff3a542aa35657
Shivom76 <shivompaul76@gmail.com>
2025-07-14 17:35:25 +0530

    final calc

--- calc/app.js
+++ calc/app.js
@@ -1,78 +1,102 @@
+let string="";
+
 console.log("page was neww")
 
-const display=document.getElementById("displayDiv")
+const display=document.getElementsByClassName("disp")
 let btnAll=document.querySelectorAll(".btn");
-const operators=["/" , "X" , "-" , "+"]
-
-let firstNum=null;
-let operator=null;
-let shouldReset=false;
+let input =document.querySelector("input")
 
 
-btnAll.forEach(btn=>{
-    btn.addEventListener("click",()=>{
+Array.from(btnAll).forEach(btn=>{
+    btn.addEventListener("click",(e)=>{
         val=btn.innerText
-
+        console.log(e.target.innerText);
 
         if(val==="AC"){
-            display.innerText=0
-            firstNum=null;
-            operator=null;
-            return;
+            input.value=0
+            string=0
         }
 
-        if(!isNaN(val)){
-            if(val==="0" || shouldReset){
-                display.innerText=val;
-                shouldReset=false;
-            }
-                
-            else{
-                display.innerText+=val
-            }
+        if(val=="="){
+            string=eval(string)
         }
-
-        else if (operators.includes(val) ){
-            let current=display.innerText; 
-
-            if(current && !operators.includes(current.slice(-1))){
-                display.innerText+=val
-                opt=val;
-            }
-        }
-        if(val==="="){
-            if (firstNum && operator ){
-                display.innerText=calc();
-                let present=display.innerText;
-                pre1=present.split(opt);
-                console.log(pre1)
-            }
-           
+        if(val!="AC"){
+            string+=val
+            input.value=string;
         }
         
-        console.log(display.innerText)
+
     })
 })
 
 
-let a,b;
-calc=(a,b,opt)=>{
-    if(opt==="+"){
-        return a+b
-    }
-    if(opt==="-"){
-        return a-b
-    }
-    if(opt==="X"){
-        return a*b
-    }
-    if(opt==="/"){
-        if (b!="0"){
-            return a/b
-        }
-        else{
-            return "Error";
-        }
-    }
-}
 
+
+
+
+
+
+
+
+// const operators=["/" , "X" , "-" , "+"]
+
+// let firstNum=null;
+// let operator=null;
+// let shouldReset=false;
+
+
+
+//         if(!isNaN(val)){
+//             if(display.innerText==="0" || shouldReset){
+//                 display.innerText=val;
+//                 shouldReset=false;
+//             }
+                
+//             else{
+//                 display.innerText+=val
+//             }
+//         }
+
+//         else if (operators.includes(val) ){
+//             let current=display.innerText; 
+
+//             if(current && !operators.includes(current.slice(-1))){
+//                 display.innerText+=val
+//                 opt=val;
+//             }
+//         }
+//         if(val==="="){
+//             if (firstNum && operator ){
+//                 display.innerText=calc();
+//                 let present=display.innerText;
+//                 pre1=present.split(opt);
+//                 console.log(pre1)
+//             }
+           
+//         }
+        
+//         console.log(display.innerText)
+//     })
+// })
+
+
+// let a,b;
+// calc=(a,b,opt)=>{
+//     if(opt==="+"){
+//         return a+b
+//     }
+//     if(opt==="-"){
+//         return a-b
+//     }
+//     if(opt==="X"){
+//         return a*b
+//     }
+//     if(opt==="/"){
+//         if (b!="0"){
+//             return a/b
+//         }
+//         else{
+//             return "Error";
+//         }
+//     }
+// }
